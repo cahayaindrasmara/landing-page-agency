0378f552a0e404da9e699687ba526b36ecc14853
cahayaindrasmara <indrasmara.cahaya27@gmail.com>
2026-07-17 15:16:09 +0700

    update some background

--- js/script.js
+++ js/script.js
@@ -2,14 +2,10 @@ document.querySelectorAll(".marquee-container").forEach((cont) => {
   const content = cont.querySelector(".marquee-content");
 
   // clone 2x seperti jQuery lama
-  cont.append(
-    content.cloneNode(true),
-    content.cloneNode(true)
-  );
+  cont.append(content.cloneNode(true), content.cloneNode(true));
 
   // kasih class marquee ke semua content
-  cont.querySelectorAll(".marquee-content")
-    .forEach(el => {
-      el.classList.add("marquee");
-    });
+  cont.querySelectorAll(".marquee-content").forEach((el) => {
+    el.classList.add("marquee");
+  });
 });
